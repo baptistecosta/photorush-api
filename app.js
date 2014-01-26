c2a0ae7cf9be88c286f5a6aca06e8a97bb02e42d
Baptiste Costa <baptiste@sefaireaider.com>
2014-01-26 23:02:15 +0100

    The basics...

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express');
-var mysql = require('mysql');
 var http = require('http');
 var path = require('path');
 var routes = require('./routes');
 var user = require('./routes/user');
-var Sequelize = require("sequelize")
+var mysql = require('mysql');
 
+/*
+var Sequelize = require("sequelize");
 var sequelize = new Sequelize("photorush", "root", "", {
 	dialect: "mysql",
 	port: 3306,
@@ -55,11 +55,25 @@ var Pix = sequelize.define('Pix', {
 	tableName: "pixes"
 });
 
-User.hasMany(Pix);
+var PixCategory = sequelize.define("PixCategory", {
+	en: Sequelize.STRING(32),
+	fr: Sequelize.STRING(32)
+}, {
+	tableName: "categories"
+});
+
 Pix.belongsTo(User);
+Pix.belongsTo(PixCategory);
+PixCategory.hasMany(Pix);
+User.hasMany(Pix);*/
+
 
 var app = express();
 // all environments
+app.use(function (req, res, next) {
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+	next();
+});
 app.set('port', process.env.PORT || 3030);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -67,6 +81,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -76,23 +91,102 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+var Bookshelf = require("bookshelf");
+Bookshelf.db = Bookshelf.initialize({
+	"client": "mysql",
+	"connection": {
+		host: "127.0.0.1",
+		user: "root",
+		password: "",
+		database: "photorush"
+//		charset: "utf8"
+	}
+});
+
+var User = Bookshelf.db.Model.extend({
+	tableName: "users",
+	pix: function() {
+		return this.hasMany(Pix);
+	}
+});
+var Users = Bookshelf.db.Collection.extend({
+	model: User
+});
+
+var Pix = Bookshelf.db.Model.extend({
+	tableName: "pixes",
+	pix_category: function() {
+		return this.belongsTo(PixCategory);
+	}
+});
+var Pixes = Bookshelf.db.Collection.extend({
+	model: Pix
+});
+
+var PixCategory = Bookshelf.db.Model.extend({
+	tableName: "categories"
+});
+var PixCategories = Bookshelf.db.Collection.extend({
+	model: PixCategory
+});
+
 app.get("/", routes.index);
 
-app.get("/users", function(req, res) {
-	User.findAll({include: [Pix]}).complete(function(err, users) {
-		if (!!err) console.log(err);
-		res.json({
-			users: users
-		});
+app.get("/user/:id", function(req, res) {
+	User.forge({id: req.params.id}).fetch().then(function(user) {
+		res.json({user: user});
+	});
+});
+app.get("/userDeep/:id", function(req, res) {
+	User.forge({id: req.params.id}).fetch({
+		withRelated: ["pix.pix_category"]
+	}).then(function(user) {
+		res.json({user: user});
 	});
 });
 
+app.get("/users", function(req, res) {
+	Users.forge().fetch().then(function(users) {
+		res.json({users: users});
+	});
+});
+app.get("/usersDeep", function(req, res) {
+	Users.forge().fetch({
+		withRelated: ["pix.pix_category"]
+	}).then(function(users) {
+		res.json({users: users});
+	});
+});
+
+app.get("/pix/:id", function(req, res) {
+	Pix.forge({id: req.params.id}).fetch({
+		withRelated: ["pix_category"]
+	}).then(function(pix) {
+		res.json({pix: pix});
+	});
+});
+app.post("pix", function() {
+	console.log(req.body);
+});
 app.get("/pixes", function(req, res) {
-	Pix.findAll({include: [User]}).complete(function(err, pixes) {
-		if (!!err) console.log(err);
-		res.json({
-			pixes: pixes
-		});
+	Pixes.forge().fetch().then(function(pixes) {
+		res.json({pixes: pixes});
+	});
+});
+app.get("/pixesDeep", function(req, res) {
+	Pixes.forge().fetch({
+		withRelated: ["user", "pix_category"]
+	}).then(function(pixes) {
+		res.json({pixes: pixes});
+	});
+});
+
+app.get("/pixCategories/:locale(en|fr)?", function(req, res) {
+	var locale = req.params.locale ? req.params.locale : "fr";
+	PixCategories.forge().fetch({
+		"columns": ["id", locale]
+	}).then(function(pixCategories) {
+		res.json({pixCategories: pixCategories});
 	});
 });
 
